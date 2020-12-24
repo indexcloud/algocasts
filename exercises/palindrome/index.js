1fa3d082da8c78a0516c43a04e82892d714628a6
Jason Deng <foralief@gmail.com>
2020-12-24 16:20:01 -0600

    Finished palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,27 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// Solution #0
+// function palindrome(str) {
+// 	// change provided string to an array by split
+// 	// use reduce method to get the reversed string by adding each element of the new array
+// 	const reversed = str.split("").reduce((rev, char) => char + rev, "");
+// 	// compare the reversed string to the provided string
+// 	// if equal return true, else return false
+// 	return reversed === str ? true : false;
+// }
+
+// Solution #1
+// function palindrome(str) {
+// 	const reversed = str.split("").reverse().join("");
+// 	return str === reversed;
+// }
+
+// Solution #2
+function palindrome(str) {
+	return str.split("").every((char, i) => {
+		return char === str[str.length - i - 1];
+	});
+}
 
 module.exports = palindrome;
