4a0a7d6bb2999c47a3eda9e0cd44cf5e0d22b31b
Jason Deng <foralief@gmail.com>
2020-12-25 12:56:19 -0600

    Finished reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,28 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// Solution #0
+function reverseInt(n) {
+	// convert interger to string
+	const reversedStr = n
+		.toString()
+		.split("")
+		.reduce((rev, char) => (char === "-" ? "" : char) + rev, ""); // 0 will be obmitted by Number() automatically
+	if (n < 0) {
+		return Number(reversedStr) * -1;
+	} else {
+		return Number(reversedStr);
+	}
+}
+
+// Solultion #1
+// function reverseInt(n) {
+// 	const reversed = n.toString().split("").reverse().join("");
+// 	// if (n < 0) {
+// 	// 	return parseInt(reversed) * -1; // parseInt() is going to stop when it reach before -, unlike Number()
+// 	// }
+// 	return parseInt(reversed) * Math.sign(n);
+// }
+// console.log(reverseInt(-98100));
 
 module.exports = reverseInt;
