e8f7e5d7da7ae0c9abd38aeebf12e2b0b7aeb35d
Jason Deng <foralief@gmail.com>
2021-03-07 16:29:38 -0600

    Finished anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,58 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// Solution #1
+// function anagrams(stringA, stringB) {
+// 	let strA = stringA.replace(/[^\w]/g, "").toLowerCase().split("").sort().join();
+// 	let strB = stringB.replace(/[^\w]/g, "").toLowerCase().split("").sort().join();
+
+// 	console.log(strA, strB);
+// 	if (strA.length !== strB.length) {
+// 		return false;
+// 	}
+
+// 	if (strA !== strB) {
+// 		return false;
+// 	}
+
+// 	return true;
+// }
+
+// Solution #2
+// function anagrams(stringA, stringB) {
+// 	const aCharMap = buildCharMap(stringA);
+// 	const bCharMap = buildCharMap(stringB);
+
+// 	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+// 		return false;
+// 	}
+
+// 	for (let char in aCharMap) {
+// 		if (aCharMap[char] !== bCharMap[char]) {
+// 			return false;
+// 		}
+// 	}
+
+// 	return true;
+// }
+
+// function buildCharMap(str) {
+// 	const charMap = {};
+
+// 	for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+// 		charMap[char] = charMap[char] + 1 || 1;
+// 	}
+
+// 	return charMap;
+// }
+
+// Solution #3
+function anagrams(stringA, stringB) {
+	return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+	return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
 
 module.exports = anagrams;
