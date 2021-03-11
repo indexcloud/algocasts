b15a8c06e3b447d9ac7f8faa4fd4490ae8fe0a7a
Jason Deng <foralief@gmail.com>
2021-03-10 21:58:29 -0600

    Finished steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,72 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Solution #1
+// function steps(n) {
+// 	// set a variabe with single #, and single space
+// 	const level = "#";
+// 	const space = " ";
+
+// 	const levelArr = [];
+// 	const spaceArr = [];
+
+// 	for (let i = 0; i < n; i++) {
+// 		spaceArr.push(space);
+// 	}
+
+// 	// console.log n times
+// 	for (let i = 0; i < n; i++) {
+// 		levelArr.push(level);
+// 		spaceArr.pop(space);
+// 		console.log(`"${levelArr.join("") + spaceArr.join("")}"`);
+// 	}
+// 	// add # each increment
+// }
+
+// steps(3);
+
+// Solution #2
+// function steps(n) {
+// 	// From 0 to n (iterate through rows)
+// 	//// Create an empty stirng, "stair"
+// 	//// From 0 to n (iterate through columns)
+// 	////// If the current column is equal to or less than the current row
+// 	//////// Add a "#" to "stair"
+// 	////// Else
+// 	//////// Add a space to "stair"
+// 	//// Console log "stair"
+// 	for (let row = 0; row < n; row++) {
+// 		let stair = "";
+
+// 		for (let column = 0; column < n; column++) {
+// 			if (column <= row) {
+// 				stair += "#";
+// 			} else {
+// 				stair += " ";
+// 			}
+// 		}
+
+// 		console.log(stair);
+// 	}
+// }
+
+// Solution #3
+function steps(n, row = 0, stair = "") {
+	if (n === row) {
+		return;
+	}
+
+	if (n === stair.length) {
+		console.log(stair);
+		return steps(n, row + 1);
+	}
+
+	if (stair.length <= row) {
+		stair += "#";
+	} else {
+		stair += " ";
+	}
+	steps(n, row, stair);
+}
 
 module.exports = steps;
