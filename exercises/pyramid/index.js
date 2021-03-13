f11a5a38e3d146596281f326ff3873f91f293c81
Jason Deng <foralief@gmail.com>
2021-03-12 23:12:34 -0600

    Finished pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,95 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Solution #1
+// function pyramid(n) {
+// 	// For rows Loop from 1 to n
+
+// 	// Create an empty string call stair
+
+// 	// For column loop from 1 to ? n + n -1
+
+// 	// add n - row - 1 space to the left and add n - row - 1 space to the right, until n - 1 = 0
+
+// 	// if certain condition, push "#" to stair
+
+// 	// else push space to stair
+
+// 	// Console log stair
+// 	for (let row = 0; row < n; row++) {
+// 		let stair = "";
+
+// 		for (let column = 0; column < 2 * n - 1; column++) {
+// 			if (column < n - row - 1 || column >= n + row) {
+// 				stair += " ";
+// 			} else {
+// 				stair += "#";
+// 			}
+// 		}
+
+// 		console.log(stair);
+// 	}
+// }
+
+// Solution #2 Recursion
+// function pyramid(n, row = 0, stair = "") {
+// 	if (n === row) {
+// 		return;
+// 	}
+
+// 	if (2 * n - 1 === stair.length) {
+// 		console.log(stair);
+// 		return pyramid(n, row + 1, (stair = ""));
+// 	}
+
+// 	if (stair.length < n - row - 1 || stair.length >= n + row) {
+// 		stair += " ";
+// 	} else {
+// 		stair += "#";
+// 	}
+
+// 	pyramid(n, row, stair);
+// }
+
+// Solution #3
+// function pyramid(n) {
+// 	const midpoint = Math.floor((2 * n - 1) / 2);
+
+// 	for (let row = 0; row < n; row++) {
+// 		let level = "";
+
+// 		for (let column = 0; column < 2 * n - 1; column++) {
+// 			if (midpoint - row <= column && midpoint + row >= column) {
+// 				level += "#";
+// 			} else {
+// 				level += " ";
+// 			}
+// 		}
+
+// 		console.log(level);
+// 	}
+// }
+
+// Solution #4
+function pyramid(n, row = 0, level = "") {
+	midpoint = Math.floor((2 * n - 1) / 2);
+
+	if (n === row) {
+		return;
+	}
+
+	if (2 * n - 1 === level.length) {
+		console.log(level);
+		return pyramid(n, row + 1, (level = ""));
+	}
+
+	if (midpoint - row <= level.length && midpoint + row >= level.length) {
+		level += "#";
+	} else {
+		level += " ";
+	}
+
+	pyramid(n, row, level);
+}
 
 module.exports = pyramid;
