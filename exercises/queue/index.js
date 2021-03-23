9589c5e9d4ea032a5207a287a1c6827a170fa341
Jason Deng <foralief@gmail.com>
2021-03-22 23:29:35 -0500

    Finished queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,26 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.data = [];
+    }
 
+    add(record) {
+        this.data.unshift(record);
+    }
+
+    remove() {
+        return this.data.pop();
+    }
+}
+
+const q = new Queue();
+q.add(1);
+q.add(2);
+q.add(3);
+q.add(4);
+
+console.log(q);
+console.log(q.remove());
 module.exports = Queue;
