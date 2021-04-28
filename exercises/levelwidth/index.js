c70348882da493839327ee3766e92a7d4fdd6f1b
Jason Deng <foralief@gmail.com>
2021-04-27 22:59:24 -0500

    Finished levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,26 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+	const counters = [0];
+
+	const stop = "s";
+
+	const arr = [root, stop];
+
+	while (arr.length > 1) {
+		const node = arr.shift();
+
+		if (node === stop) {
+			counters.push(0);
+			arr.push(stop);
+		} else {
+			arr.push(...node.children); // spread operator, node.children is an array
+			counters[counters.length - 1]++;
+		}
+	}
+
+	return counters;
+}
 
 module.exports = levelWidth;
